59dfaa6e13f4ea96d87c54b769745f14ed66d18b
manoj-m-01 <manoj904378@gmail.com>
2022-10-31 17:05:22 +0530

    update finish till 6080

---
+++ Problems/0094 Binary Tree Inorder Traversal/Solution.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function (root) {
+    const stack = [];
+    const res = [];
+
+    while (root || stack.length) {
+        if (root) {
+            stack.push(root);
+            root = root.left;
+        } else {
+            root = stack.pop();
+            res.push(root.val);
+            root = root.right;
+        }
+    }
+
+    return res;
+};
